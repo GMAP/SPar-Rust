fdc05d686052a7f7d9bddae54dc80929d6eeba1c
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2023-02-24 11:53:07 -0300

    testing with a normal vector

--- tests/semantics/sort.rs
+++ tests/semantics/sort.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), String> {
     }
 
     let other_vec = Vec::new();
-    let other_vec = to_stream!(INPUT(vec: Vec<u32>), OUTPUT(other_vec: Vec<Vec<u32>>), {
+    let other_vec = to_stream!(INPUT(vec: Vec<u32>), OUTPUT(other_vec: Vec<u32>), {
         let mut vec_slice = &mut vec[0..];
         for _ in 0..9 {
             let split = vec_slice.split_at_mut(10000);
@@ -18,21 +18,17 @@ fn main() -> Result<(), String> {
                 input.sort();
                 let sorted = input;
             });
-            STAGE(INPUT(sorted: Vec<u32>), OUTPUT(other_vec: Vec<Vec<u32>> ), {
-                other_vec.push(sorted);
+            STAGE(INPUT(sorted: Vec<u32>), OUTPUT(other_vec: Vec<u32>), {
+                other_vec.extend(&sorted);
             });
         }
     });
 
-    assert_eq!(other_vec.len(), 9);
-    let mut counter: usize = 0;
+    assert_eq!(other_vec.len(), 90000);
     let mut cur: u32 = 0;
-    for vec in other_vec {
-        for i in vec {
-            assert!(cur <= i, "cur: {cur}, i: {i}, at index: {counter}");
-            cur = i;
-            counter += 1;
-        }
+    for (index, i) in other_vec.iter().enumerate() {
+        assert!(cur <= *i, "cur: {cur}, i: {i}, at index: {index}");
+        cur = *i;
     }
 
     Ok(())
